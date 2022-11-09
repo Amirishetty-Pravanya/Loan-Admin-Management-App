4a8c25f84adae20c5cc1d994bb08ad54150b6066
Amirishetty-Pravanya <61456669+Amirishetty-Pravanya@users.noreply.github.com>
2022-11-09 09:44:32 +0530

    connecting frontend

--- src/main/java/com/example/loanadminmanagement/controller/AdminController.java
+++ src/main/java/com/example/loanadminmanagement/controller/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/admin")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
 
     @Autowired
